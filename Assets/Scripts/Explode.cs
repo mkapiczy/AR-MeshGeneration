c687cbf70635f1e0c8fc31236c3196ece8801ac7
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-15 13:51:34 +0100

    Initial project setup

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+	
+	void Start() {
+		Debug.Log ("Explosion");
+		ParticleSystem exp = GetComponent<ParticleSystem>();
+		exp.Play();
+	}
+}
---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVForUnity;
+
+public class EarthScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Mat mat = new Mat ();
+	}
+
+
+		
+}
